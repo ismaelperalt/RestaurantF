0be36d2a07971d3aa8f9df2a4ae126c74a3e7c19
ismaelperalt <ismaelsaguay90@gmail.com>
2026-03-05 20:08:22 -0500

    feat: animación

--- src/components/TableCard.jsx
+++ src/components/TableCard.jsx
@@ -1,6 +1,13 @@
 import StatusBadge from "./StatusBadge";
 
 export default function TableCard({ order, onClick }) {
+
+  const progressClass = {
+    pending: "progress-pending",
+    preparing: "progress-preparing",
+    served: "progress-served"
+  };
+
   return (
     <div className="card" onClick={onClick}>
       
@@ -12,6 +19,11 @@ export default function TableCard({ order, onClick }) {
       <p className="card-products">
         {order.items.length} productos
       </p>
+
+      {/* progress */}
+      <div className="progress-bar">
+        <div className={`progress ${progressClass[order.status]}`}></div>
+      </div>
 
     </div>
   );
